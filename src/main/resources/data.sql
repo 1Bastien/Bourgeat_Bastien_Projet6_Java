7251a3c0e8a85660de90ded375c47815de92d713
Bastien <bastien.brgt@protonmail.com>
2024-01-11 23:58:26 +0100

    Update script SQL.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,9 +7,19 @@ CREATE TABLE user (
     role VARCHAR(5) NOT NULL,
     balance DECIMAL(10,2) DEFAULT 0.00 NOT NULL,
     billing_address VARCHAR(150) NOT NULL,
-    contact_list_json JSON,
+    bankAccountNumber VARCHAR(30),
     
     CONSTRAINT ck_user_balance CHECK (balance > 0)
+);
+
+CREATE TABLE contact_list (
+    user_id BIGINT NOT NULL,
+    contact_email VARCHAR(30) NOT NULL,
+    
+    PRIMARY KEY (user_id, contact_email),
+    
+    CONSTRAINT fk_contact_list_user_id FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT fk_contact_list_contact_email FOREIGN KEY (contact_email) REFERENCES user(email)
 );
 
 CREATE TABLE transaction (
@@ -17,6 +27,7 @@ CREATE TABLE transaction (
     sender_id BIGINT NOT NULL,
     receiver_id BIGINT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
+    fee DECIMAL(10,2) NOT NULL,
     transaction_date DATETIME NOT NULL,
     
     CONSTRAINT fk_transaction_sender_id FOREIGN KEY (sender_id) REFERENCES user (user_id),
@@ -25,11 +36,9 @@ CREATE TABLE transaction (
     CONSTRAINT ck_transaction_amount CHECK (amount > 0)
 );
 
-INSERT INTO my_user (first_name, last_name, email, password, role, balance, billing_address, contact_list_json) VALUES 
-	('John', 'Doe', 'john.doe@example.com', 'hashed_password', 'USER', 100.00, '123 Main St', '["contact1@example.com", "contact2@example.com"]');
-	('Smith', 'Alice', 'alice.smith@example.com', 'hashed_password_2', 'ADMIN', 150.00, '456 Oak Ave', '["contact1@example.com", "contact2@example.com"]');
+INSERT INTO user (last_name, first_name, email, password, role, balance, billing_address, bankAccountNumber) VALUES 
+	('Doe', 'John', 'john.doe@example.com', 'hashed_password', 'USER', 1000.00, '123 Main St', '1234567890');
+	
+INSERT INTO contact_list (user_id, contact_email) VALUES (1, 'jane.smith@example.com');
 
-INSERT INTO user_contacts (user_id, contact_id) VALUES (1, 2);
-
-INSERT INTO transaction (sender_id, receiver_id, amount, transaction_date) VALUES (1, 2, 50.00, '2022-01-01 12:00:00');
-    
+INSERT INTO transaction (sender_id, receiver_id, amount, fee, transaction_date) VALUES (1, 2, 50.00, 1.00, '2024-01-12 08:30:00');
