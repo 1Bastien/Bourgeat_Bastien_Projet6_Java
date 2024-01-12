c602e100011b59c231571bd22c53d285c0c2ddb7
Bastien <bastien.brgt@protonmail.com>
2024-01-12 13:31:57 +0100

    Add About,Login and Home page.

---
+++ src/main/java/com/paymybuddy/app/controller/AboutController.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+}
---
+++ src/test/java/com/paymybuddy/app/controller/LoginControllerTest.java
@@ -0,0 +1,24 @@
+package com.paymybuddy.app.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testLoginPage() throws Exception {
+		mockMvc.perform(get("/login")).andExpect(status().isOk()).andExpect(view().name("login"));
+	}
+}
