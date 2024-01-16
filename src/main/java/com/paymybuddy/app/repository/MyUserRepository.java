c6d87e53291baf6c48396d8bad93e2e55db0ff53
Bastien <bastien.brgt@protonmail.com>
2024-01-16 11:34:29 +0100

    Utilisation du CrudRepository à la place du JpaRepository.

--- src/main/java/com/paymybuddy/app/repository/MyUserRepository.java
+++ src/main/java/com/paymybuddy/app/repository/MyUserRepository.java
@@ -2,13 +2,13 @@ package com.paymybuddy.app.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.paymybuddy.app.model.MyUser;
 
 @Repository
-public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+public interface MyUserRepository extends CrudRepository<MyUser, Long> {
 
 	public Optional<MyUser> findByEmail(String email);
 }
--- src/main/java/com/paymybuddy/app/repository/TransactionRepository.java
+++ src/main/java/com/paymybuddy/app/repository/TransactionRepository.java
@@ -2,14 +2,14 @@ package com.paymybuddy.app.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.paymybuddy.app.model.MyUser;
 import com.paymybuddy.app.model.Transaction;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 
 	List<Transaction> findBySender(MyUser sender);
 
